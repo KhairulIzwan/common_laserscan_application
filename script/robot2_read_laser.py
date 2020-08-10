c4f2e21837107a8ebb51d801844dba637859b334
KhairulIzwan <wansnap@gmail.com>
2020-08-11 03:57:23 +0800

    Updated on 08-10-2020-23:17:50

--- script/robot2_read_laser.py
+++ script/robot2_read_laser.py
@@ -8,43 +8,32 @@
 ## Email: {wansnap@gmail.com}
 ################################################################################
 
-# import the necessary Python packages
-#from __future__ import print_function
-#import sys
-#import cv2
-#import time
-
-# import the necessary ROS packages
-#from std_msgs.msg import String
-#from sensor_msgs.msg import Image
-#from sensor_msgs.msg import CameraInfo
-
-#from cv_bridge import CvBridge
-#from cv_bridge import CvBridgeError
-
-#import math
 from sensor_msgs.msg import LaserScan
-#from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist
 
 import rospy
 
 import numpy as np
+import math
 
 class LaserPreview:
 	def __init__(self):
-
-#		self.bridge = CvBridge()
-#		self.image_received = False
-		self.laser_received = False
 
 		rospy.logwarn("[Robot2] LaserRead Node [ONLINE]...")
 
 		# rospy shutdown
 		rospy.on_shutdown(self.cbShutdown)
+				
+		self.laser_received = False
+		self.move = Twist()
 			
 		# Subscribe to LaserScan msg
 		self.laser_topic = "/scan_robot2"
 		self.laser_sub = rospy.Subscriber(self.laser_topic, LaserScan, self.cbLaser)
+		
+		# Publish to Twist msg
+		twist_topic = "/cmd_vel_robot2"
+		self.twist_pub = rospy.Publisher(twist_topic, Twist, queue_size=10)
 
 		# Allow up to one second to connection
 		rospy.sleep(1)
@@ -54,6 +43,7 @@ class LaserPreview:
 
 		try:
 			self.scanValue = msg.ranges
+			self.minAng = msg.angle_min
 
 		except KeyboardInterrupt as e:
 			print(e)
@@ -61,28 +51,127 @@ class LaserPreview:
 		if self.scanValue is not None:
 			self.laser_received = True
 		else:
-			self.laser_received = False		
+			self.laser_received = False
+			
+	def cbScan(self):
+	
+		scan_filter = []
+
+		samples = len(self.scanValue)
+		samples_view = 1024
+
+		if samples_view > samples:
+			samples_view = samples
+
+		if samples_view is 1:
+			scan_filter.append(self.scanValue[len(self.scanValue) // 2])
+
+		else:
+			right_lidar_samples = self.scanValue[(len(self.scanValue) // 2):len(self.scanValue)]
+			left_lidar_samples = self.scanValue[0:(len(self.scanValue) // 2) - 1]
+			scan_filter.extend(left_lidar_samples + right_lidar_samples)
+
+		for i in range(len(scan_filter)):
+			if scan_filter[i] == float('Inf'):
+				scan_filter[i] = 3.5
+			elif math.isnan(scan_filter[i]):
+				scan_filter[i] = 0
+
+		return scan_filter		
 
 	# Print info
 	def cbLaserInfo(self):
 		if self.laser_received:
-#			rospy.loginfo([self.scanValue[0], self.scanValue[512]])
-			center = len(self.scanValue) // 2
-			if np.isnan(self.scanValue[center]):
-				pass
-			else:
-				if self.scanValue[center] > 0.6:
-#					rospy.loginfo("[Angle: 0]: %.4f" % (self.scanValue[0]))
-					pass
-				else:
-					rospy.logwarn("[Robot2] OBSTACLE! [Angle: 0]: %.4f" % (self.scanValue[center]))
+			lidar_distances = self.cbScan()
+			
+			center = lidar_distances[len(lidar_distances) // 2]
+			right = lidar_distances[1 * (len(self.scanValue) // 3)]
+			left = lidar_distances[2 * (len(self.scanValue) // 3)]
+
+			rospy.loginfo("L: %.4f, C: %.4f, R: %.4f" % (left, center, right))
+				
+			if right > 0.6 and right > left:
+				self.pubMoveR()
+				rospy.logwarn("Right")
+			elif left > 0.6 and right < left:
+				self.pubMoveL()
+				rospy.logwarn("Left")
+			elif right < 0.6 and left < 0.6 and center > 0.6:
+				self.pubMove()
+				rospy.logwarn("Center")
+			elif right < 0.6 and left < 0.6 and center < 0.6:
+				self.pubStop()
+				rospy.logwarn("Stop")
 		else:
 			rospy.logerr("No Laser Reading")
+			self.pubStop()
+			rospy.logwarn("Stop")
+			
+		# Allow up to one second to connection
+		rospy.sleep(0.1)
 
+	# Publishing the Twist msg (STOP)
+	def pubStop(self):
+		self.move.linear.x = 0
+		self.move.linear.y = 0
+		self.move.linear.z = 0
+		
+		self.move.angular.x = 0
+		self.move.angular.y = 0
+		self.move.angular.z = 0
+		
+		self.twist_pub.publish(self.move)
+		
+	# Publishing the Twist msg (MOVE)
+	def pubMove(self):
+		self.move.linear.x = 0.05
+		self.move.linear.y = 0.00
+		self.move.linear.z = 0.00
+
+		self.move.angular.x = 0.00
+		self.move.angular.y = 0.00
+		self.move.angular.z = 0.00
+
+		self.twist_pub.publish(self.move)
+		
+	# Publishing the Twist msg (MOVE R)
+	def pubMoveR(self):
+		self.move.linear.x = 0.05
+		self.move.linear.y = 0.00
+		self.move.linear.z = 0.00
+
+		self.move.angular.x = 0.00
+		self.move.angular.y = 0.00
+		self.move.angular.z = 0.2
+
+		self.twist_pub.publish(self.move)
+		
+	# Publishing the Twist msg (MOVE L)
+	def pubMoveL(self):
+		self.move.linear.x = 0.05
+		self.move.linear.y = 0.00
+		self.move.linear.z = 0.00
+
+		self.move.angular.x = 0.00
+		self.move.angular.y = 0.00
+		self.move.angular.z = -0.2
+
+		self.twist_pub.publish(self.move)
+		
 	# rospy shutdown callback
 	def cbShutdown(self):
 
 		rospy.logerr("[Robot2] LaserRead Node [OFFLINE]...")
+		
+		self.move.linear.x = 0.00
+		self.move.linear.y = 0.00
+		self.move.linear.z = 0.00
+		
+		self.move.angular.x = 0.00
+		self.move.angular.y = 0.00
+		self.move.angular.z = 0.00
+		
+		self.twist_pub.publish(self.move)
 
 if __name__ == '__main__':
 
